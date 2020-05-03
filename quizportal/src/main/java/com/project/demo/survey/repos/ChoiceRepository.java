5ac9a1d3a9a64e081e61ef13fdbde499f9618dc1
Shreyajha98 <jha.shreya98@gmail.com>
2020-05-03 17:21:01 +0530

    First commit

---
+++ quizportal/src/main/java/com/project/demo/survey/repos/ChoiceRepository.java
@@ -0,0 +1,12 @@
+package com.project.demo.survey.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.demo.survey.entities.Choice;
+
+
+public interface ChoiceRepository extends JpaRepository<Choice, Integer>{
+	public Choice findById(int id);
+} 
+
+
